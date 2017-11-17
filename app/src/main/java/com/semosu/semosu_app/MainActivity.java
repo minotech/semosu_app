a29991c1e25d26810634c5317c2400e82ac4515f
minho <minotech1@hanmail.net>
2017-11-18 01:56:31 +0900

    뒤로가기 버튼 완료

--- app/src/main/java/com/semosu/semosu_app/MainActivity.java
+++ app/src/main/java/com/semosu/semosu_app/MainActivity.java
@@ -1,24 +1,30 @@
 package com.semosu.semosu_app;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private WebView mWebView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        WebView mWebView = (WebView) findViewById(R.id.webView);
+        mWebView = (WebView) findViewById(R.id.webView);
         mWebView.setInitialScale(1);
 
         WebSettings webSettings = mWebView.getSettings();
@@ -51,6 +57,34 @@ public class MainActivity extends AppCompatActivity {
 
 
     @Override
+    public boolean onKeyDown(int keyCode, android.view.KeyEvent event){
+        if((keyCode == KeyEvent.KEYCODE_BACK) && mWebView.canGoBack() ){
+            mWebView.goBack();
+            return true;
+        }
+
+        //백할 페이가 없다면
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && (mWebView.canGoBack() == false)){
+           // Toast.makeText(this, "버튼 클릭 이벤트", Toast.LENGTH_SHORT).show();
+
+            //다이아로그박스 출력
+            new AlertDialog.Builder(this)
+                    .setTitle("프로그램 종료")
+                    .setMessage("프로그램을 종료하시겠습니까?")
+                    .setPositiveButton("예", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            android.os.Process.killProcess(android.os.Process.myPid());
+                        }
+                    })
+                    .setNegativeButton("아니오",  null).show();
+        }
+
+        return super.onKeyDown(keyCode, event);    }
+
+
+
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
@@ -71,6 +105,9 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
+
 
 
 
